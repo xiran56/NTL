ad393539e6a45cff53b438e97493fe5c99dcaa3d
xiran56 <xiranrtx@gmail.com>
2023-12-16 09:39:56 +0500

    Подготовка к написанию тестов

--- include/ntl/DigitsIterator.hh
+++ include/ntl/DigitsIterator.hh
@@ -2,13 +2,13 @@
 
 #include <limits>
 
-#include "impl/digits.hh"
+#include "impl/NumberDigitConvTraits.hh"
 
 namespace ntl {
-    template<class View, class ViewTraits>
+    template<class View, class T, bool minusCounted = false, class ConvTraits = impl::traits::NumberDigitConvTraits<T, minusCounted>>
     class DigitsIterator {
         static constexpr std::size_t UNDEFINED { std::numeric_limits<std::size_t>::max() };
     public:
-        
+     
     };
 }
--- include/ntl/impl/NumberDigitConvTraits.hh
+++ include/ntl/impl/NumberDigitConvTraits.hh
@@ -7,14 +7,15 @@
 #include <cmath>
 #include <type_traits>
 
-namespace ntl::impl {
+namespace ntl::impl::traits {
     template<class T, bool minusCounted = false>
     struct NumberDigitConvTraits : public Traits {
         static_assert(std::is_integral_v<T>, "Invalid forwarded type!");
-        static_assert(!(std::is_unsigned_v<T> && minusCounted), "Invalid template argumends");
+        static_assert(!(std::is_unsigned_v<T> && minusCounted), "Invalid template arguments");
     public:
         using SizeType = std::size_t;
         using BaseType = std::uint8_t;
+        using DigitType = std::uint8_t;
 
         static SizeType countDigits(T n, BaseType base) {
             SizeType result { 0 };
@@ -31,8 +32,19 @@ namespace ntl::impl {
         }
     
         template<class View>
-        inline static auto getDigit(T n, SizeType index) {
+        inline static View getDigit(T n, SizeType index, BaseType base) {
+            if constexpr (minusCounted && std::is_same_v<View, char>)
+                if (n < 0 && index == 1)
+                    return '-';
 
+            const DigitType intView = (n / std::pow(base, index)) % base;
+
+            if constexpr (std::is_integral_v<View>)
+                return intView;
+            else if constexpr (std::is_same_v<View, char>)
+                return intView + '0';
+            else
+                static_assert(alwaysFalse<View>, "Unsupported view!");
         }
     };
 }
